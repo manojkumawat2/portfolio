d0e49491dbab3386b7216224fb3eade67c711433
Manoj Kumawat <manoj@internshala.com>
2022-01-27 13:17:19 +0530

    Added more features to contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './Contact.css';
 
 function Contact() {
     const [email, setEmail] = useState();
     const [name, setName] = useState();
     const [message, setMessage] = useState();
-    const [isFormSubmitting, setIsFormSubmitting] = useState(0);
+    const [formMessage, setFormMessage] = useState({
+        'status': '',
+        'message': ''
+    });
+    const [formSubmittingStatus, setFormSubmittingStatus] = useState(0);
     const Spineer = () => {
         return (
             <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
@@ -24,9 +29,30 @@ function Contact() {
         setMessage(event.target.value);
     }
 
-    const handleFormSubmit = (event) => {
-        setIsFormSubmitting(1);
+    const handleFormSubmit = async (event) => {
         event.preventDefault();
+        setFormSubmittingStatus(1);
+        const url = 'https://murlapi.herokuapp.com/index.php?/api/contact';
+        const data = new FormData();
+        data.append('name', name);
+        data.append('email', email);
+        data.append('message', message);
+
+        const response =  await axios.post(url, data);
+        
+        if(response.data.statusCode === 200) {
+            setFormMessage({
+                'status': 'success',
+                'message': response.data.messages[0]
+            });
+        } else {
+            setFormMessage({
+                'status': 'error',
+                'message': "Please try again later!"
+            });
+        }
+
+        setFormSubmittingStatus(0);
     }
 
     return (
@@ -35,17 +61,20 @@ function Contact() {
             <form className="contact-form shadow-sm p-3" onSubmit={handleFormSubmit}>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" placeholder="name@example.com" value={email} onChange={handleEmailChange} />
+                    <input type="email" class="form-control" id="email" placeholder="name@example.com" value={email} onChange={handleEmailChange} required/>
                 </div>
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="name" placeholder="Enter Name" value={name} onChange={handleNameChange} />
+                    <input type="text" class="form-control" id="name" placeholder="Enter Name" value={name} onChange={handleNameChange} required/>
                 </div>
                 <div class="mb-3">
                     <label for="message" class="form-label">Email address</label>
-                    <textarea className="form-control" id="message" placeholder="Enter Message" rows="6" value={message} onChange={handleMessageChange} />
+                    <textarea className="form-control" id="message" placeholder="Enter Message" rows="6" value={message} onChange={handleMessageChange} required/>
                 </div>
-                <button type="submit" class="btn btn-primary">{isFormSubmitting === 0 ? 'Send' : "Sending"} {isFormSubmitting === 0 ? <i class="bi bi-send"></i> : <Spineer />}</button>
+                <button type="submit" class={formSubmittingStatus === 0 ? 'btn btn-primary' : "btn btn-primary disabled"}>{formSubmittingStatus === 0 ? 'Send' : "Sending"} {formSubmittingStatus === 0 ? <i class="bi bi-send"></i> : <Spineer />}</button>
+                <div className={`mt-2 text-center ${formMessage.status === 'success' ? 'text-success' : 'text-danger'}`}>
+                    {formMessage.message}
+                </div>
             </form>
         </div>
     );
